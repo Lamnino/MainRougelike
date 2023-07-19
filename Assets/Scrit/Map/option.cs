766cb950b5728e8f776b3e98d361a064fa258744
Lamnino <phanduclam02@gmail.com>
2023-07-19 22:18:37 +0700

    Lam RandomMap

--- Assets/Scrit/Map/option.cs
+++ Assets/Scrit/Map/option.cs
@@ -11,9 +11,11 @@ public class option : MonoBehaviour
     public GameObject[] Right;
     public GameObject[] Left;
     public GameObject[] bottom;
-    public Transform[] Road = new Transform[100];
+    Transform[] Road = new Transform[30];
     Transform newMap;
     int rdn;
+    int maxX =30;
+    int maxY = 20;
 
     private void Start()
     {
@@ -21,6 +23,10 @@ public class option : MonoBehaviour
     }
     public Transform GetMapWithNum(Transform ObviouMap, int num)
     {
+        if (num == 1 && ObviouMap.position.x <= 0) return null;
+        if (num == 4 && ObviouMap.position.y <= 0) return null;
+        if (num == 3 && ObviouMap.position.x >= maxX) return null;
+        if (num == 2 && ObviouMap.position.y >= maxY) return null;
         switch (num)
         {
             case 1:
@@ -29,7 +35,7 @@ public class option : MonoBehaviour
                 break;
             case 2:
                 rdn = Random.Range(0, bottom.Length-1);
-                newMap = Instantiate(bottom[rdn], ObviouMap.position + new Vector3(0, 8,0), Quaternion.identity, transform).transform;
+                newMap = Instantiate(bottom[rdn], ObviouMap.position + new Vector3(0, 7,0), Quaternion.identity, transform).transform;
                 break;
             case 3:
                 rdn = Random.Range(0, Left.Length - 1);
@@ -37,15 +43,15 @@ public class option : MonoBehaviour
                 break;
             case 4:
                 rdn = Random.Range(0, above.Length - 1);
-                newMap = Instantiate(above[rdn], ObviouMap.position + new Vector3(0, -8, 0), Quaternion.identity, transform).transform;
+                newMap = Instantiate(above[rdn], ObviouMap.position + new Vector3(0, -7, 0), Quaternion.identity, transform).transform;
                 break;
         }
         int j = 0;
-        while (Road[j] !=null)
+        while (Road[j] != null)
         {
             if (newMap.position == Road[j].position)
             {
-                Debug.Log(Road[j]);
+                Destroy(newMap.gameObject);
                 return null;
             }
             j++;
@@ -58,36 +64,5 @@ public class option : MonoBehaviour
         if (NumGate == 1) return above.Length;
         return 0;
     }
-    //case -1:
-    //            rdn = Random.Range(0, above.Length-1);
-    //            Debug.Log("yes");
-    //            GameObject newmap11 = Instantiate(above[rdn], obvious);
-    //RandomMap script = newmap11.GetComponent<RandomMap>();
-    //script.SpawnNextMap(-1);
-    //            break;
-
-    //        case 1:
-    //            rdn = Random.Range(0, Right.Length-1);
-    //            GameObject newmap1 = Instantiate(Right[rdn], obvious.position + new Vector3(-12, 0, 0), Quaternion.identity, transform);
-    //script= newmap1.GetComponent<RandomMap>();
-    //            script.SpawnNextMap(3);
-    //            break;
-    //        case 2:
-    //            rdn = Random.Range(0, bottom.Length-1);
-    //            GameObject newmap2 = Instantiate(bottom[rdn], obvious.position + new Vector3(0, 8, 0), Quaternion.identity, transform);
-    //script = newmap2.GetComponent<RandomMap>();
-    //            script.SpawnNextMap(4);
-    //            break;
-    //        case 3:
-    //            rdn = Random.Range(0, Left.Length-1);
-    //            GameObject newmap3 = Instantiate(Left[rdn], obvious.position + new Vector3(12, 0, 0), Quaternion.identity, transform);
-    //script = newmap3.GetComponent<RandomMap>();
-    //            script.SpawnNextMap(1);
-    //            break;
-    //        case 4:
-    //            rdn = Random.Range(0, above.Length-1);
-    //            GameObject newmap4 = Instantiate(above[rdn], obvious.position + new Vector3(0, -8, 0), Quaternion.identity, transform);
-    //script = newmap4.GetComponent<RandomMap>();
-    //            script.SpawnNextMap(2);
-    //            break;
+    
 }
